9074e4bea10b294c225464d2a27b5f8b871b5c13
sos-dll <53872123+sos-dll@users.noreply.github.com>
2020-10-31 02:19:01 +0100

    Add project files.

---
+++ core2fx/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace core2fx
+{
+    public static class CoreClass
+    {
+        [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvStdcall) })]
+        public static int MyCoreRoutine()
+        {
+            Console.WriteLine(".NET Core -> .NET Framework");
+            return 5_00_00_02;
+        }
+    }
+}
---
+++ fx2core/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using static Vanara.PInvoke.Kernel32;
+
+namespace fx2core
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var core2fxlib = LoadLibrary("core2fxNE.dll");
+            if (core2fxlib == null)
+            {
+                Console.Error.WriteLine("Make sure you compiled core2fx and copied all files next to the fx2core app.");
+                return;
+            }
+            var preload_runtime = GetProcAddress(core2fxlib, "preload_runtime");
+            var preloadRuntime = Marshal.GetDelegateForFunctionPointer<PreloadRuntime>(preload_runtime);
+            Console.WriteLine(".NET Framework -> .NET Core");
+            preloadRuntime();
+            var my_core_runtime = GetProcAddress(core2fxlib, "MyCoreRoutine");
+            var myCoreRuntime = Marshal.GetDelegateForFunctionPointer<MyCoreRoutine>(my_core_runtime);
+            var result = myCoreRuntime();
+            Console.WriteLine("Done = {0}", result);
+            Console.ReadLine();
+        }
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Ansi, SetLastError = false)]
+        public delegate void PreloadRuntime();
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Ansi, SetLastError = false)]
+        public delegate int MyCoreRoutine();
+    }
+}
